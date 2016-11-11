39b2964b89ff71debbb79cdd6b2ce41fccdf450b
crhilditch20 <crhilditch20@gmail.com>
2016-11-11 20:26:10 +0000

    commit changes to bar class and spec

--- bar.rb
+++ bar.rb
@@ -2,8 +2,9 @@ class Bar
 
 attr_reader :drinks_menu
 
-def initialize
+def initialize(till)
   @drinks_menu = []
+  @till = till
 end
 
 def add_drink_to_menu(drink)
@@ -14,13 +15,20 @@ def count_drinks()
   @drinks_menu.count
 end
 
-def serve_drink(drink_choice)
-  for drink in @drinks_menu
-    if drink.type == drink_choice
-      return "£#{drink.price} please"
-    end
-  return "Sorry, dinnae have it, pal."
-  end
+def count_cash()
+  @till.total_cash()
 end
 
+def serve_drink(customer, drink_choice)
+    for drink in @drinks_menu
+      if drink.type == drink_choice
+        @till.total_cash += drink.price
+        customer.cash -= drink.price
+      else
+        return "Sorry, we don't have that drink"
+      end
+  end
 end
+ 
+end
+
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -2,15 +2,21 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../bar.rb')
 require_relative('../drink.rb')
+require_relative('../till.rb')
+require_relative('../guest.rb')
 
 class TestBar < Minitest::Test
 
 def setup
-  @bar = Bar.new()
+  @bar_till = Till.new(100)
+
+  @bar = Bar.new(@bar_till)
 
   @drink1 = Drink.new("ale", 3)
   @drink2 = Drink.new("lager", 3)
   @drink3 = Drink.new("wine", 4)
+
+  @customer = Guest.new("Bob", 40)
 
 end
 
@@ -20,11 +26,21 @@ def test_can_add_drinks
   assert_equal(1, result)
 end
 
+def test_count_cash
+  result = @bar.count_cash
+  assert_equal(100, result)
+end
+
 def test_can_serve_drinks
   @bar.add_drink_to_menu(@drink3)
-  result = @bar.serve_drink("wine")
-  assert_equal("£4 please", result)
+  @bar.serve_drink(@customer, "wine")
+  assert_equal(104, @bar.count_cash)
+  assert_equal(36, @customer.cash)
+  # assert_equal("Sorry, we don't have that drink", result)
 end
   
 
 end
+
+
+
