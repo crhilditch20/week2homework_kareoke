59a246bebf7b8ab4af4223962109510bd927a4aa
crhilditch20 <crhilditch20@gmail.com>
2016-11-11 19:31:53 +0000

    commit change to drink_spec

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -5,8 +5,13 @@ require_relative('../drink.rb')
 class TestDrink < Minitest::Test
 
 def test_drink_type
-  drink1 = Drink.new("Ale", 3.50)
+  drink1 = Drink.new("Ale", 4)
   assert_equal("Ale", drink1.type)
+end
+
+def test_drink_price
+  drink1 = Drink.new("Ale", 4)
+  assert_equal(4, drink1.price)
 end
 
 end
